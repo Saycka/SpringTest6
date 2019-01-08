17036d30071a829f096a3725ce7f58792140af00
Andrey <a>
2019-01-08 14:07:53 +0300

    commit 1

---
+++ src/main/java/ru/manyatkin/SpringTest6/SpringTest6Application.java
@@ -0,0 +1,14 @@
+package ru.manyatkin.SpringTest6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTest6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTest6Application.class, args);
+	}
+
+}
+
---
+++ src/test/java/ru/manyatkin/SpringTest6/SpringTest6ApplicationTests.java
@@ -0,0 +1,17 @@
+package ru.manyatkin.SpringTest6;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTest6ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
